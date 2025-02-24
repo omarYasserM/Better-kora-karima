a694a3dfba3ece1495d90c288964a864062eec5a
Omar Yasser <62072111+omarYasserM@users.noreply.github.com>
2025-02-24 11:17:33 +0200

    fix build

--- components/beneficiary-form.tsx
+++ components/beneficiary-form.tsx
@@ -12,7 +12,6 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { ImageUpload } from "@/components/ui/image-upload"
 import { useOptions } from "@/providers/options-provider"
 
 const formSchema = z.object({
